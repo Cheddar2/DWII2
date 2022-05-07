9c85fa8defa3db9f16d42233b9671a0227bdd8cb
Cheddar2 <cesaraugustoschenattodossantos@gmail.com>
2022-05-06 21:34:20 -0300

    2 files added, 1 deleted

---
+++ exercico032.js
@@ -0,0 +1,23 @@
+const tarefas = [
+  {
+    titulo: 'passear com o catiorro',
+    concluida: false,
+  },
+  {
+    titulo: 'comprar couve',
+    concluida: false,
+  },
+  { titulo: 'lavar loça', concluida: true },
+];
+const concluida = { concluida } == true;
+{
+return concluida;
+console.log(concluida);}
+
+const concluida = (({concluida}) == false)
+return concluida;
+console.log(concluida);
+
+
+
+
