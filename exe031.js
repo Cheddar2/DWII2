1154a6a57b43fef0300ccbb359f34dfe9fdb1c56
Cheddar2 <cesaraugustoschenattodossantos@gmail.com>
2022-05-13 21:08:32 -0300

    1 file added, 1 deleted

---
+++ exe031.js
@@ -0,0 +1,36 @@
+const filmes = [
+  {
+    titulo: 'adam sandler e a sociedade secreta',
+    ano: 2055,
+
+  },
+  {
+    titulo: 'adam sandler de volta ao jogo',
+    ano: 205,
+
+  },
+  {
+    titulo: 'adam sandler duro de matar 4 ',
+    ano: 20555,
+
+
+  },
+  
+]
+//const nomelancamento = filmes.map((filme, index, array) => {
+ // return{
+ //   titulo: filme.titulo,
+ //   ano: filme.tiltulo
+// };
+//});
+
+const nomelancamento = filmes.map(({titulo, ano})  => ({
+  titulo,
+  ano,
+
+  
+}));
+
+console.log(nomelancamento);
+//obs nao consegui enviar antes da correcao pq nao consegui conectar o git a tempo.
+
--- index.js
+++
@@ -1,3 +0,0 @@
-const minhafucao = (nome, idade) => {
-
-}
