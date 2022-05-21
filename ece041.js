d4c94a5d76469dd9c8b7a97c45fc49387705e5b3
Cheddar2 <cesaraugustoschenattodossantos@gmail.com>
2022-05-20 22:31:30 -0300

    2 files added

---
+++ ece041.js
@@ -0,0 +1,25 @@
+const alunos = [
+  {
+    nome: 'Mathias',
+    notas:[
+      { cadeira: 'Dw2', nota: 10 },
+      { cadeira: 'Bd2', nota: 7,} 
+    ],
+  },
+  {
+    nome: 'luisa',
+    notas:[
+      { cadeira: 'Dw2', nota: 8 },
+      { cadeira: 'Bd2', nota: 8,} 
+    ],
+  },
+  {
+    nome: 'Manoel',
+    notas:[
+      { cadeira: 'Dw2', nota: 10 },
+      { cadeira: 'Bd2', nota: 3,} 
+    ],
+  }
+]
+
+const Sus = alunos.sort((notas) => { (B - A)})
---
+++ index.js
@@ -0,0 +1,34 @@
+const arquivos = [
+  {
+    id: 1,
+    nome: 'Meu trabson v1',
+    excluido:false,
+
+  },
+  {
+    id: 2,
+    nome: 'meu trabson v2',
+    excluido: false,
+
+  },
+  {
+    id: 3,
+    nome: 'meu trabson v3',
+    excluido: false,
+    
+  },{
+    id: 4,
+    nome: 'meu trabson v4',
+    excluido: true,
+    
+  },
+  
+]
+//const ablubluble = arquivos.forEach(({excluido}) => ({ if (excluido === true){
+//  ecluido === false,
+//}
+//
+//
+//})),
+
+const ablubluble = arquivos.forEach((id,nome,excluido) => {})
